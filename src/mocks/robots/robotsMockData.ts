a2709741361c8ada855aacb5063fff9177ef6cb3
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-13 13:06:42 +0200

    Add robotsDataMock for testing purposes

---
+++ src/mocks/robots/robotsMockData.ts
@@ -0,0 +1,24 @@
+import { RobotStructure } from "../../types";
+
+const robotsMockData: RobotStructure[] = [
+  {
+    _id: "asidjdi-2736-adakshda",
+    name: "R2D2",
+    speed: 2,
+    resistence: 10,
+    dateOfCreation: "10/03/2000",
+    imageUrl:
+      "https://cdn.s7.shopdisney.eu/is/image/DisneyStoreES/461015528352",
+  },
+  {
+    _id: "asdasd-53466-jhkyuifg",
+    name: "WALL-E",
+    speed: 10,
+    resistence: 3,
+    dateOfCreation: "02/03/2010",
+    imageUrl:
+      "https://static.wikia.nocookie.net/disney/images/9/9c/Profile_-_WALL-E.png",
+  },
+];
+
+export default robotsMockData;
