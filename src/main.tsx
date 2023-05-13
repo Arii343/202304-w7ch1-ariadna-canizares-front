ad1eed3e86a470b2f0a2a05cf6750dd33d33bda7
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-13 22:17:47 +0200

    Add appRouter and NotFoutn route

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import { store } from "./store";
 import GlobalStyle from "./styles/GlobalStyle/GlobalStyle";
 import "@fontsource/inter";
+import appRouter from "./routers/appRouter/appRouter";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={appRouter} />
       <GlobalStyle />
     </Provider>
   </React.StrictMode>
---
+++ src/routers/appRouter/appRouter.tsx
@@ -0,0 +1,23 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+import App from "../../components/App/App";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "*",
+        element: <p>Page not found, plese try another one!</p>,
+      },
+      {
+        path: "hello",
+        element: <p>Page not found, plese try another one!</p>,
+      },
+    ],
+  },
+];
+
+const appRouter = createBrowserRouter(routes);
+
+export default appRouter;
